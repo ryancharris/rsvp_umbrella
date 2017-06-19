86d65893ad2528d7ff33c98d1c89529d51a82cc2
Ryan Harris <ryan.harris@revzilla.com>
2017-06-19 07:16:46 -0400

    Configure Ecto and RSVP Repo

--- apps/rsvp/config/config.exs
+++ apps/rsvp/config/config.exs
@@ -28,3 +28,10 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+config :rsvp, Rsvp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "rsvp",
+  username: "postgres",
+  password: "postgres"
+
+config :rsvp, ecto_repos: [Rsvp.Repo]
--- apps/rsvp/lib/rsvp.ex
+++ apps/rsvp/lib/rsvp.ex
@@ -1,18 +1,14 @@
 defmodule Rsvp do
-  @moduledoc """
-  Documentation for Rsvp.
-  """
+  use Application
 
-  @doc """
-  Hello world.
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
 
-  ## Examples
+    children = [
+      worker(Rsvp.Repo, [])
+    ]
 
-      iex> Rsvp.hello
-      :world
-
-  """
-  def hello do
-    :world
+    opts = [strategy: :one_for_one, name: Rsvp.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
