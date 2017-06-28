66432320e864da174c7c8f12a180cef769acac41
Ryan Harris <ryan.harris@revzilla.com>
2017-06-27 21:09:53 -0400

    Create Events model

---
+++ apps/rsvp/lib/events/events.ex
@@ -0,0 +1,12 @@
+defmodule Rsvp.Events do
+  use Ecto.Schema
+
+  schema "events" do
+    field :title, :string
+    field :location, :string
+    field :date, Ecto.DateTime
+
+    timestamps
+  end
+
+end
