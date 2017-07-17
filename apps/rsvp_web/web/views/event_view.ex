3e5af62fbb28e5c6386ec84453121122aa377d9d
Ryan Harris <ryan.harris@revzilla.com>
2017-07-16 20:27:11 -0400

    Define EventView helper methods to format date and time values in the template

--- apps/rsvp_web/web/views/event_view.ex
+++ apps/rsvp_web/web/views/event_view.ex
@@ -3,7 +3,19 @@ defmodule RsvpWeb.EventView do
 
   def format_date(date) do
     {{y, m, d}, _} = Ecto.DateTime.to_erl(date)
-    "#{d}/#{m}/#{y}"
+    "#{single_to_double_digits(m)}/#{single_to_double_digits(d)}/#{single_to_double_digits(y)}"
+  end
+
+  def format_time(date) do
+    {_, {h, m, s}} = Ecto.DateTime.to_erl(date)
+    "#{single_to_double_digits(h)}:#{single_to_double_digits(m)}"
+  end
+
+  def single_to_double_digits(num) do
+    case num < 10 do
+      true -> "0#{num}"
+      false -> num
+    end
   end
 
 end
